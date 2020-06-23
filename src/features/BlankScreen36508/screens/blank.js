ed7ab1521655e36a624376b624df77befe57c2e2
Crowdbotics <team@crowdbotics.com>
2020-06-23 07:04:52 +0000

    Update components

--- src/features/BlankScreen36508/screens/blank.js
+++ src/features/BlankScreen36508/screens/blank.js
@@ -167,6 +167,19 @@ export class _Blank extends React.Component {
       >
         Button
       </Button>
+      <Slider
+        value={50}
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        disabled={false}
+        style={{
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderStyle: "solid"
+        }}
+      />
     </View>
   )
 }
